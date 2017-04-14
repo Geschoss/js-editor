4d4a9fe034588dbfba6c44e9990bbd2848ca59bd
geschoss <q11254@gmail.com>
2017-04-14 20:06:57 +0700

    home

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -5,6 +5,31 @@ import { SidebarToggleService } from '../services/sidebar-toggle.service';
 import { JsonCntService } from '../services/json-cnt.service';
 import { Model, TextObj } from '../model';
 
+const MODEL = {
+  "backgroundUrl": "http://nakolenke.club/uploads/posts/2016-09/1473248830_kotiki06.jpg",
+  "x-size": 1500,
+  "y-size": 500,
+  "content": [
+    {
+      "text": "first",
+      "x": 100,
+      "y": 59,
+      "size": 54,
+      "color": "#515151",
+      "font": "arial, verdana, sans-serif"
+    },
+    {
+      "text": "second",
+      "x": 200,
+      "y": 209,
+      "size": 154,
+      "color": "#515151",
+      "font": "arial, verdana, sans-serif"
+    }
+  ]
+};
+
+
 
 @Component({
   selector: 'app-sidebar',
@@ -12,7 +37,7 @@ import { Model, TextObj } from '../model';
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit, AfterViewInit {
-    private initJson: String = '';
+    private initJson: String = JSON.stringify(MODEL, undefined, 2);
   	private isOpen$: Observable<boolean>;
     private invalid: boolean = false;
   	constructor(private sidebarToggleService: SidebarToggleService,
